b0f70e0fff5250f6cd7b2264598a87be29cc6514
pxlnght <pxl.nght@gmail.com>
2025-01-18 15:04:23 -0500

    #38 Create Hero NPC
    Hero that walks right at constant speed.

---
+++ idiot.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@export var speed: float = 100.0
+
+func _physics_process(delta: float) -> void:
+	# Move the NPC to the right at 'speed' pixels per second
+	position.x += speed * delta
