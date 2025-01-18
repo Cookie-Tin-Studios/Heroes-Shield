68845d0cbeb77caad7a523f8bb9fd3951464491e
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-18 15:04:38 -0600

    add pause/unpause

---
+++ pause_node.gd
@@ -0,0 +1,19 @@
+extends Node
+
+# This node is necessary to pause and unpause the game because all processing is stopped when 
+# a node's ProcessMode is `inherit`, but this node is set to `always`
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(_event): 
+	if Input.is_action_pressed("Pause"):
+		pause_game()
+
+func pause_game():
+	get_tree().paused = not get_tree().paused
