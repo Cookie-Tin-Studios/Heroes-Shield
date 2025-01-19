31aa26ca018424a90fc55ab5cfa73af3297557c6
pxlnght <pxl.nght@gmail.com>
2025-01-19 17:17:42 -0500

    MVP game over menu

---
+++ scripts/menu/game_over.gd
@@ -0,0 +1,24 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_back_to_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_ez_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
