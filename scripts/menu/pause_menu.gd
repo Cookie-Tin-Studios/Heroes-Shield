98787362e695f74e423dfc9a319f8f692c8ff66d
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-20 17:59:59 -0600

    add a delay after pausing for resume via pause keybind

--- scripts/menu/pause_menu.gd
+++ scripts/menu/pause_menu.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+const resumeDelay = 300
+var timePaused = Time.get_ticks_msec()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,8 +15,9 @@ func _process(_delta: float) -> void:
 func _input(_event): 
 	if Input.is_action_pressed("Pause"):
 		if not get_tree().paused:
+			timePaused = Time.get_ticks_msec()
 			pause_game()
-		else:
+		elif check_time_delay(Time.get_ticks_msec()):
 			resume_game()
 
 func _on_resume_pressed() -> void:
@@ -26,6 +29,9 @@ func _on_settings_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
+func check_time_delay(newPause: int):
+	var diff = newPause - timePaused
+	return diff > resumeDelay
 
 func pause_game():
 	visible = true
