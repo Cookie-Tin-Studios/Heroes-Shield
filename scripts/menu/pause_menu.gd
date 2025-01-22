6767eeeb43e423ec6d75c6ea94d9846e7ab2eff6
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-21 18:21:11 -0600

    better pause nodes

--- scripts/menu/pause_menu.gd
+++ scripts/menu/pause_menu.gd
@@ -5,6 +5,7 @@ var timePaused = Time.get_ticks_msec()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	visible = false
 	pass # Replace with function body.
 
 
