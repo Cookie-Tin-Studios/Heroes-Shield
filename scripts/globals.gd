3be72799c9f8cf92bcb337dacafed3c735d374b7
Miguel Rodriguez <miguelrodriguez1598@gmail.com>
2025-01-21 21:08:53 -0500

    Add progress bar (#70)
    * Add progress bar
    
    * Ignore .ds_store
    
    * Remove unused asset
    
    * Remove ds store files from repo

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,18 +1,39 @@
 extends Node
 
-# Notify on coin change.
 signal coins_changed(new_amount)
+signal level_progress_changed(new_amount)
+signal max_level_progress_changed(new_amount)
 
-# Global coin variable w setter and getter.
+var _coins: int = 0
+var _level_progress: int = 0
+var _max_level_progress: int = 1000
+
 var coins: int:
-	get():
-		return coins
+	get:
+		return _coins
 	set(value):
-		coins = value
-		emit_signal("coins_changed", coins)
+		_coins = value
+		emit_signal("coins_changed", _coins)
 
-func _ready():
-	print("Globals script initialized. Starting with coins =", coins)
+var level_progress: int:
+	get:
+		return _level_progress
+	set(value):
+		_level_progress = value
+		emit_signal("level_progress_changed", _level_progress)
+		
+var max_level_progress: int:
+	get:
+		return _max_level_progress
+	set(value):
+		_max_level_progress = value
+		emit_signal("max_level_progress_changed", _max_level_progress)
+
+func _ready() -> void:
+	print("Globals script initialized. Starting with coins =", _coins)
 
 func add_coins(amount: int) -> void:
 	coins += amount
+
+func add_progress(amount: int) -> void:
+	level_progress += amount
