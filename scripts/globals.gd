14af268ee7f959f5c95816137ca9c010397de262
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-24 18:49:46 -0600

    remove redundant classes

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -39,17 +39,17 @@ func add_progress(amount: int) -> void:
 	level_progress += amount
 	
 var upgrades = [
-	UpgradeCategory.new("Movement", [
-		MovementUpgrade.new("Speed 1", "You move faster, what did you expect", 1, 0.1),
-		MovementUpgrade.new("Speed 2", "You move faster, electric boogaloo", 2, 0.1)
+	UpgradeCategory.new_upgrade_category_scene("Movement", [
+		Upgrade.new_upgrade_scene("Speed 1", "You move faster, what did you expect", 1),
+		Upgrade.new_upgrade_scene("Speed 2", "You move faster, electric boogaloo", 2)
 	]),
-	UpgradeCategory.new("Category 2", [
-		Upgrade.new("Upgrade 1", "Yeah it's an upgrade", 1),
-		Upgrade.new("Upgrade 2", "Yeah it's an upgrade", 2)
+	UpgradeCategory.new_upgrade_category_scene("Category 2", [
+		Upgrade.new_upgrade_scene("Upgrade 1", "Yeah it's an upgrade", 1),
+		Upgrade.new_upgrade_scene("Upgrade 2", "Yeah it's an upgrade", 2)
 	]),
-	UpgradeCategory.new("Category 3", [
-		Upgrade.new("Upgrade 1", "Yeah it's an upgrade", 1),
-		Upgrade.new("Upgrade 2", "Yeah it's an upgrade", 2)
+	UpgradeCategory.new_upgrade_category_scene("Category 3", [
+		Upgrade.new_upgrade_scene("Upgrade 1", "Yeah it's an upgrade", 1),
+		Upgrade.new_upgrade_scene("Upgrade 2", "Yeah it's an upgrade", 2)
 	])
 ]
 var active_upgrades: Array[Upgrade] = []
