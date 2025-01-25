d6261cda644c8a916f557188f200f9f5428bbc5a
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-24 18:49:46 -0600

    remove redundant classes

--- scripts/menu/upgrades/upgrade_category_scene.gd
+++ scripts/menu/upgrades/upgrade_category_scene.gd
@@ -1,18 +1,23 @@
+class_name UpgradeCategory
 extends Control
 
-var category: UpgradeCategory = UpgradeCategory.new("test", [])
-const upgrade_scene: PackedScene = preload("res://scenes/menu/upgrades/upgrade.tscn")
-
 signal purchased(Upgrade)
+var Name: String
+var Upgrades: Array[Upgrade]
+const UPGRADE_CATEGORY = preload("res://scenes/menu/upgrades/upgrade_category.tscn")
 
-# Called when the node enters the scene tree for the first time.
+static func new_upgrade_category_scene(name: String, upgrades: Array[Upgrade]) -> UpgradeCategory:
+	var new_category = UPGRADE_CATEGORY.instantiate()
+	new_category.Name = name
+	new_category.Upgrades = upgrades
+	return new_category
+
+
 func _ready() -> void:
-	$"Category Name".text = category.Name
-	for upgrade in category.Upgrades:
-		var newUpgrade = upgrade_scene.instantiate()
-		newUpgrade.upgrade = upgrade
-		newUpgrade.purchased.connect(_on_upgrade_purchased)
-		$HBoxContainer/upgrade_list.add_child(newUpgrade)
+	$"Category Name".text = Name
+	for upgrade in Upgrades:
+		$HBoxContainer/upgrade_list.add_child(upgrade)
+		upgrade.purchased.connect(_on_upgrade_purchased)
 
 func _on_upgrade_purchased(upgrade: Upgrade) -> void:
 	purchased.emit(upgrade)
