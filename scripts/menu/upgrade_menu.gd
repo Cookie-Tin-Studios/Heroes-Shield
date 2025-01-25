f8c3b85fc53f54283a378b688c1113f92a8d7c07
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-24 18:49:46 -0600

    remove redundant classes

--- scripts/menu/upgrade_menu.gd
+++ scripts/menu/upgrade_menu.gd
@@ -8,10 +8,8 @@ var upgrades = Globals.upgrades
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for category in upgrades:
-		var newCategory = upgrade_category.instantiate()
-		newCategory.category = category
-		newCategory.purchased.connect(_on_upgrade_purchased)
-		$upgrade_columns.add_child(newCategory)
+		category.purchased.connect(_on_upgrade_purchased)
+		$upgrade_columns.add_child(category)
 
 func _process(_delta) -> void:
 	if Input.is_action_pressed("ui_previous_menu"):
--- scripts/menu/upgrades/upgrade_scene.gd
+++ scripts/menu/upgrades/upgrade_scene.gd
@@ -1,19 +1,30 @@
+class_name Upgrade
 extends VBoxContainer
 
-var upgrade: Upgrade
 @onready var description: Label = $description
 @onready var upgrade_button: Button = $upgrade
+const UPGRADE = preload("res://scenes/menu/upgrades/upgrade.tscn")
 
+# === Data Structure ===
 signal purchased(Upgrade)
+var Name: String
+var Description: String
+var Cost: int
 
-# Called when the node enters the scene tree for the first time.
+static func new_upgrade_scene(name: String, desc: String, cost: int) -> Upgrade:
+	var new_upgrade = UPGRADE.instantiate()
+	new_upgrade.Name = name
+	new_upgrade.Description = desc
+	new_upgrade.Cost = cost
+	return new_upgrade
+
+# === Scene Methods ===
 func _ready() -> void:
-	upgrade_button.text = "(%s) %s" % [upgrade.Cost, upgrade.Name]
-	description.text = upgrade.Description
-	
+	upgrade_button.text = "(%s) %s" % [Cost, Name]
+	description.text = Description
 
 func _on_click() -> void:
 	print_debug("purchasing upgrade")
-	purchased.emit(upgrade)
+	purchased.emit(self)
 	# TODO: Coin Balance check
 	# TODO: Coin Balance subtract
