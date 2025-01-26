4ced25834740fd08632d56c2d8c6b6c87f5693fb
Mrod1598 <miguelrodriguez1598@gmail.com>
2025-01-26 12:25:04 -0500

    Add audio queues

--- scripts/effects/trail.gd
+++ scripts/effects/trail.gd
@@ -3,6 +3,10 @@ class_name Trail
 
 var queue: Array
 @export var MAX_LENGTH: int = 30
+@export var trail_width: float = 2.0 
+
+func _ready() -> void:
+	self.width = trail_width
 
 func _process(_delta) -> void:
 	var pos = _get_position()
