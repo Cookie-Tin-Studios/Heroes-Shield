b8e718f25c174c1d1fdab5280ff478cf8468eec0
pxlnght <pxl.nght@gmail.com>
2025-01-26 14:17:05 -0500

    HELP

--- scripts/rail.gd
+++ scripts/rail.gd
@@ -23,8 +23,7 @@ var end_pos: Vector2:
 	set(position):
 		_end_pos = Vector2(position)
 		update_points()
-
-
+		
 func update_points() -> void:
 	var line_2d: Line2D = $Line2D
 	line_2d.clear_points()
@@ -38,17 +37,3 @@ func update_points() -> void:
 	segment_shape.a = start_pos
 	segment_shape.b = end_pos
 	collision_shape.shape = segment_shape
-	
-	
-	
-	#update_collision()
-#
-#func update_collision() -> void:
-	#var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-	#var line_2d: Line2D = $Line2D
-	#var new_rect = RectangleShape2D.new()
-	#new_rect.size = Vector2(width, _start_pos.distance_to(_end_pos))
-	#var r = new_rect.get_rect()
-	#collision_shape_2d.shape = new_rect
-	#collision_shape_2d.position = Vector2(_start_pos.x + _end_pos.x, _start_pos.y + _end_pos.y) / 2
-	#collision_shape_2d.rotate(_start_pos.angle_to_point(_end_pos))
