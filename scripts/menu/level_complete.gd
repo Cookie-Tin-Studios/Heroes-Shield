ad67b953ef3be539e1fd9afef4deaca5e79b28fc
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-29 21:28:24 -0600

    who dun did that

--- scripts/menu/level_complete.gd
+++ scripts/menu/level_complete.gd
@@ -6,3 +6,7 @@ func _process(_delta) -> void:
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
+
+
+func _on_upgrades_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/upgrade_menu.tscn")
