72d625a25b5062b8d7bd1d9243a979fef6613d5e
Matthew Sawatzky <msawatzky75@gmail.com>
2025-01-29 20:46:59 -0600

    add credits screen

--- scripts/menu/level_complete.gd
+++ scripts/menu/level_complete.gd
@@ -1,11 +1,11 @@
 extends MarginContainer
 
+func _ready() -> void:
+	
+
 func _process(_delta) -> void:
 	if Input.is_action_just_pressed("ui_previous_menu"):
 		_on_main_menu_pressed()
-
-func _on_upgrades_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu/upgrade_menu.tscn")
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
